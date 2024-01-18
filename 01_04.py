4f25cc2b646c7315c94a96e0762d569eb0929d6b
AryanTKh <156952854+AryanTKh@users.noreply.github.com>
2024-01-18 21:29:38 +0330

    Update 01_04.py

--- 01_04.py
+++ 01_04.py
@@ -1,10 +1,10 @@
-a, b = map(int, input().split())
+a , b = map( int , input().split() )
 
 def is_prime(num):
-    if num < 2:
+    if num < 2 :
         return False
-    for i in range(2, int(num**0.5) + 1):
-        if num % i == 0:
+    for i in range(2, int ( num ** 0.5 ) + 1 ) :
+        if num % i == 0 :
             return False
     return True
 
@@ -12,5 +12,5 @@ if a <= b:
     count = sum(1 for i in range(a, b+1) if is_prime(i))
     print(f"main order - amount: {count}")
 else:
-    count = sum(1 for i in range(b, a+1) if is_prime(i))
+    count = sum ( 1 for i in range( b , a + 1 ) if is_prime(i) )
     print(f"reverse order - amount: {count}")
